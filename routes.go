1ae8d512cf4691e1cbd52cc8b974e5b4e69673f8
YUN-TA HSIEH <s3584414@student.rmit.edu.au>
2018-09-28 16:29:53 +1000

    bar chart

--- routes.go
+++ routes.go
@@ -38,7 +38,7 @@ func initializeRoutes() {
 		c.JSON(http.StatusOK, stock)
 	})
 	router.GET("/barChart2", func(c *gin.Context) {
-		stock := CalGICS(a, b)
+		stock := CalRegion(a, b)
 		c.JSON(http.StatusOK, stock)
 	})
 	router.GET("/barChart3", func(c *gin.Context) {
